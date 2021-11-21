0eba9357f0cc940346e152dd846a5acdaa4484be
himax <himax82@gmail.com>
2021-11-21 21:32:54 +0800

    2. Асинхронное взаимодействие [#458497]

---
+++ src/main/java/ru/job4j/kafka_service/KafkaServiceApplication.java
@@ -0,0 +1,23 @@
+package ru.job4j.kafka_service;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@EnableKafka
+@SpringBootApplication
+public class KafkaServiceApplication {
+
+    @KafkaListener(topics= {"pasport"})
+    public void msgListener(ConsumerRecord<String, String> input){
+        System.out.println("accepting " + input.key());
+        System.out.println(input.value());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaServiceApplication.class, args);
+    }
+
+}
